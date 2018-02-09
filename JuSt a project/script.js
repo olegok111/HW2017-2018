9ae9e685b9d3dcc40819ae2a9176f73bf8d72bc6
olegok111 <olegok2003@mail.ru>
2018-02-09 21:36:54 +0300

    v1 Yes!!! It works!!!
    There is a bouncing ball. Actually BOUNCING!

--- JuSt a project/script.js
+++ JuSt a project/script.js
@@ -2,39 +2,37 @@ var c = document.getElementById("my_canvas");
 var ctx = c.getContext("2d");
 var cx = 40;
 var cy = 40;
-var op = 0;
 var g = 10;
+var dt = 0.3; // this argument changes mass indeed
+var op = 0;
 var v = 0;
-var dt = 0.2;
+var mode = 'down';
 
 function draw() {
-  ctx.beginPath();
-  ctx.arc(cx,cy,40,0,2*Math.PI);
-  ctx.stroke();
+	op++;
+	ctx.beginPath();
+	ctx.arc(cx,cy,40,0,2*Math.PI);
+	ctx.stroke();
+	if (op % 2 === 0) {
+	  ctx.clearRect(0, 0, 400, 400);
+	}
+	if (cy >= 360) {
+		mode = 'up';
+	}
+	if (v === 0 && cy < 360) {
+		mode = 'down';
+	} else if (v === 0 && cy >= 360) {
+		mode = 'stop';
+	}
+	if (mode === 'down') {
+		v += g*dt;
+		cy += v*dt/2;
+	} else if (mode === 'up') {
+		v -= g*dt;
+		cy -= v*dt/2;
+	}
+	console.log(v);
+	window.requestAnimationFrame(draw);
 }
 
-function motion_su() {
-  ctx.clearRect(0, 0, 400, 400);
-  if (cy >= 360) {
-	v -= g*dt;
-	cy -= v*dt;
-  } else {
-	v += g*dt;
-	cy += v*dt;
-  }
-  window.requestAnimationFrame(draw);
-}
-
-function pausecomp(millis)
-{
-    var date = new Date();
-    var curDate = null;
-    do { curDate = new Date(); }
-    while(curDate-date < millis);
-}
-
-// wszixudfoiqewfgyo9zxy9tg
 draw();
-motion_su();
-pausecomp(1000);
-motion_su();
